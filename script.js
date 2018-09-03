051017b83ccb2744eee354ece5760832ff85d955
Blythe Totsch <uncblythe1@gmail.com>
2018-09-03 11:04:07 -0500

    added gradient to checkerboard

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    let body = document.getElementsByTagName('body')[0];
+    body.style.background = "linear-gradient(160deg, lavender, lightblue)";
     for (let i= 0; i < 72; i ++){
      const tile = document.createElement('div') 
         tile.className = 'square'    
@@ -7,23 +9,23 @@ document.addEventListener('DOMContentLoaded', function () {
         tile.style.float = 'left';
         tile.style.paddingBottom = '11.1%';
         tile.style.height = '11.1%';
-        
-        document.body.appendChild(tile);
     
+        if (i % 2 === 0) {
+            tile.style.backgroundColor = "rgba(17, 33, 99, " + (.02 * i) + ")";
+          } else {
+            tile.style.backgroundColor = "rgba(130, 133, 199, " + (.02 * i) + ")";
+          }
 
-    // if (i % 2 === 0) {
-    //     tile.style.backgroundColor = 'black';
-    // } else {
-    //     tile.style.backgroundColor = 'red';
+    body.appendChild(tile);
+
+    
+    // function randomColor() {
+    //     let a = Math.ceil(Math.random() * 255);
+    //     let b = Math.ceil(Math.random() * 255);
+    //     let c = Math.ceil(Math.random() * 255);
+    //     tile.style.backgroundColor = 'rgb('+a+','+b+','+c+')';
     // }
-
-    function randomColor() {
-        let a = Math.ceil(Math.random() * 255);
-        let b = Math.ceil(Math.random() * 255);
-        let c = Math.ceil(Math.random() * 255);
-        tile.style.backgroundColor = 'rgb('+a+','+b+','+c+')';
-    }
-    window.setInterval(randomColor, 2000);
+    // window.setInterval(randomColor, 2000);
 
 }
 
