cab18443672836adec5fc1c3068846133b5c89a9
Blythe Totsch <uncblythe1@gmail.com>
2018-09-03 10:32:50 -0500

    completed red/black checkerboard

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Your JS goes here
+document.addEventListener('DOMContentLoaded', function () {
+
+    for (let i= 0; i < 72; i ++){
+     const tile = document.createElement('div') 
+        tile.className = 'square'    
+        tile.style.width = '11.1%';
+        tile.style.float = 'left';
+        tile.style.paddingBottom = '11.1%';
+        tile.style.height = '11.1%';
+        
+        document.body.appendChild(tile);
+    
+
+    if (i % 2 === 0) {
+        tile.style.backgroundColor = 'black';
+    } else {
+        tile.style.backgroundColor = 'red';
+    }
+
+}
+
+
+
+
+})
+
